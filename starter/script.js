89ce5ebcf53e3aa32b9857e6ad9513c4af62eac0
nickncb96 <nicholasncb12@hotmail.com>
2024-01-09 14:33:42 +0000

    adding numeric array characters

--- starter/script.js
+++ starter/script.js
@@ -25,4 +25,7 @@ var specialCharacters = [
   .'				
   ];	
 
-  
+  // Array of numeric characters to be included in password				
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];	
+
+
