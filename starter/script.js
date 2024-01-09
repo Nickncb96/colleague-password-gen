8f273a49cfea9533dfabd6add24e0d81b43fc8f7
nickncb96 <nicholasncb12@hotmail.com>
2024-01-09 14:36:16 +0000

    adding upper case array characters

--- starter/script.js
+++ starter/script.js
@@ -33,4 +33,7 @@ var lowerCasedCharacters = [
   a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'				
   ];
 
-  
+  // Array of uppercase characters to be included in password				
+var upperCasedCharacters = [				
+  A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'				
+  ];	
